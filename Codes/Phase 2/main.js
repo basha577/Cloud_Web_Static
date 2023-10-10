e2ca8b0ffe975878a6d3ac57886edbd1c37260e7
basha577 <146541675+basha577@users.noreply.github.com>
2023-10-10 21:44:25 +0530

    Programs for Phase 2

---
+++ Codes/Phase 2/main.js
@@ -0,0 +1,69 @@
+
+const facebookBtn = document.querySelector(".facebook-btn");
+const twitterBtn = document.querySelector(".twitter-btn");
+const pinterestBtn = document.querySelector(".pinterest-btn");
+const linkedinBtn = document.querySelector(".linkedin-btn");
+const whatsappBtn = document.querySelector(".whatsapp-btn");
+
+function init() {
+  const pinterestImg = document.querySelector(".pinterest-img");
+
+  let postUrl = encodeURI(document.location.href);
+  let postTitle = encodeURI(document.getElementById("title").textContent);
+  let postImg = encodeURI(pinterestImg.src);
+
+  facebookBtn.setAttribute(
+    "href",
+    `https://www.facebook.com/sharer.php?u=${postUrl}`
+  );
+
+  twitterBtn.setAttribute(
+    "href",
+    `https://twitter.com/share?url=${postUrl}&text=${postTitle}`
+  );
+
+  pinterestBtn.setAttribute(
+    "href",
+    `https://pinterest.com/pin/create/bookmarklet/?media=${postImg}&url=${postUrl}&description=${postTitle}`
+  );
+
+  linkedinBtn.setAttribute(
+    "href",
+    `https://www.linkedin.com/shareArticle?url=${postUrl}&title=${postTitle}`
+  );
+
+  whatsappBtn.setAttribute(
+    "href",
+    `https://wa.me/?text=${postTitle} ${postUrl}`
+  );
+}
+
+init();
+
+commentInput.addEventListener("keyup", (event) => {if (event.key == "Enter") {addComment();}});
+
+function addComment() {
+  const commentInput = document.getElementById("commentInput");
+  const commentText = commentInput.value.trim();
+
+  if (commentText !== "") {
+      const commentsContainer = document.getElementById("comments");
+      const commentDiv = document.createElement("div");
+      commentDiv.classList.add("comment");
+      commentDiv.innerHTML = `
+          <div>${commentText}</div>
+          <div class="comment-actions">
+              <span class="material-symbols-outlined" onclick="deleteComment(this)">delete</span>
+          </div>
+      `;
+      commentsContainer.appendChild(commentDiv);
+      commentInput.value = "";
+      
+      window.scrollTo(0, document.body.scrollHeight);
+  }
+}
+
+function deleteComment(button) {
+  const commentDiv = button.closest(".comment");
+  commentDiv.remove();
+}
